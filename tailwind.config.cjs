635c0973f1075030ed92aeca678f8c3719dad6ec
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2022-12-08 07:54:28 +0600

    feat: added icon to site footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   safelist: ["hidden", "block", "bg-[#523857]", "grid-cols-2", "grid-cols-3"],
   theme: {
+    screens: {
+      xs: "324px",
+      ...defaultTheme.screens,
+    },
     extend: {
       maxWidth: {
         blog: "832px",
